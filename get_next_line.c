ee26611f38fb235e5382de84a23024be09d18682
Nikolaos  Goulios <ngoulios@c1r6p5.hive.fi>
2024-05-22 12:19:45 +0300

    stracture

--- get_next_line.c
+++ get_next_line.c
@@ -1,18 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ngoulios <ngoulios@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/19 19:45:15 by ngoulios          #+#    #+#             */
-/*   Updated: 2024/05/22 12:18:03 by ngoulios         ###   ########.fr       */
+/*   Created: 2024/05/22 12:19:05 by ngoulios          #+#    #+#             */
+/*   Updated: 2024/05/22 12:19:07 by ngoulios         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
-
-char    *get_next_line(int fd);
-
-#endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,18 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ngoulios <ngoulios@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/19 19:45:15 by ngoulios          #+#    #+#             */
-/*   Updated: 2024/05/22 12:18:03 by ngoulios         ###   ########.fr       */
+/*   Created: 2024/05/22 12:18:52 by ngoulios          #+#    #+#             */
+/*   Updated: 2024/05/22 12:18:53 by ngoulios         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
-
-char    *get_next_line(int fd);
-
-#endif
