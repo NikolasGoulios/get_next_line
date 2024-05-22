d43693333954a96cc52ea76d487eec52399619d9
Nikolaos  Goulios <ngoulios@c1r6p5.hive.fi>
2024-05-22 14:18:01 +0300

    main function prototype

--- get_next_line.c
+++ get_next_line.c
@@ -6,16 +6,38 @@
 /*   By: ngoulios <ngoulios@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 12:19:05 by ngoulios          #+#    #+#             */
-/*   Updated: 2024/05/22 12:58:12 by ngoulios         ###   ########.fr       */
+/*   Updated: 2024/05/22 14:15:12 by ngoulios         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-//char    *get_next_line(int fd)
-//{
-	//fd is the file descriptior
-	// Open the file or create one
+char    *get_next_line(int fd)
+{
+	static char *left_c;
+	char *line;
+	char buffer;
+
+	buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+	{
+		free(left_c);
+		free(buffer);
+		left_c = NULL;
+		buffer = NULL;
+		return (line);
+	}
+	if (!buffer)
+		return(NULL);
+	line = fill_line_buf(fd, left_c, buffer);
+	free(buffer);
+	buffer = 0;
+	if (!line)
+		return(NULL);
+	left_c = set_line(line);
+	return (line);
+}
 
 	// read the file 
 	// return the next line
@@ -26,12 +48,4 @@
 	 // buf again 0 to start string other charaters
 
 	 // stash will be freed but only the 
-//}
-
-int main()
-{
-	int fd;
-
-	fd = open("text.txt", O_RDONLY -O_CREAT);
-	return 0;
-}
+	 
