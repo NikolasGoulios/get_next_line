2ce33aa1fbf940f0fee2932b8993d9905f436188
Nikolaos  Goulios <ngoulios@c1r6p5.hive.fi>
2024-05-27 11:49:37 +0300

    from scratch

--- get_next_line.h
+++ get_next_line.h
@@ -6,23 +6,25 @@
 /*   By: ngoulios <ngoulios@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/19 19:45:15 by ngoulios          #+#    #+#             */
-/*   Updated: 2024/05/22 14:20:13 by ngoulios         ###   ########.fr       */
+/*   Updated: 2024/05/27 11:48:58 by ngoulios         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <fcntl.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 16
+# endif
+
 # include <stdlib.h>
 # include <unistd.h>
-# include <stdio.h>
 
-char    *get_next_line(int fd);
+char	*get_next_line(int fd);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s1);
-size_t	ft_strlen(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(const char *s1, const char *s2);
 
+size_t	ft_strlen(const char *s);
 #endif
