502702c5467668992e1cdfb1604e976071be1e0e
Nikolaos  Goulios <ngoulios@c1r6p4.hive.fi>
2024-07-24 12:37:30 +0300

    combine functions for norminette limit

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,27 +6,21 @@
 /*   By: ngoulios <ngoulios@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 12:18:52 by ngoulios          #+#    #+#             */
-/*   Updated: 2024/07/24 11:43:05 by ngoulios         ###   ########.fr       */
+/*   Updated: 2024/07/24 12:35:23 by ngoulios         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlen(const char *s)
 {
-	size_t	srclen;
+	int	i;
 
-	srclen = ft_strlen(src);
-	if (srclen + 1 < dstsize)
-		ft_memcpy (dst, src, srclen + 1);
-	else if (dstsize != 0)
-	{
-		ft_memcpy (dst, src, dstsize - 1);
-		dst[dstsize - 1] = 0;
-	}
-	return (srclen);
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
 }
-
 char	*ft_strdup(const char *s1)
 {
 	char	*dest;
@@ -59,33 +53,36 @@ char	*ft_strchr(const char *s, int c)
 		return ((char *) s);
 	return (0);
 }
-size_t	ft_strlen(const char *s)
-{
-	int	i;
 
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
-}
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char    *ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*substr;
-	size_t	new_len;
+    char    *substr;
+    size_t  new_len;
+    size_t  i;
 
+    if (!s)
+        return (NULL);
 	if ((unsigned int)ft_strlen(s) <= start)
-		return (ft_strdup(""));
-	new_len = ft_strlen(s) - start;
-	if (new_len < len)
-		len = new_len;
-	substr = (char *)malloc(sizeof(char) * (len + 1));
-	if (!s)
-		return (NULL);
-	if (!substr)
-		return (NULL);
-	ft_strlcpy(substr, s + start, len + 1);
+        return (ft_strdup(""));
+
+    new_len = ft_strlen(s) - start;
+    if (new_len < len)
+        len = new_len;
+
+    substr = (char *)malloc(sizeof(char) * (len + 1));
+    if (!substr)
+        return (NULL);
+
+    i = 0;
+    while (i < len)
+    {
+        substr[i] = s[start + i];
+        i++;
+    }
+    substr[len] = '\0';
 	return (substr);
 }
+
 char	*ft_strjoin(const char *s1, const char *s2)
 {
 	int		i;
@@ -105,22 +102,4 @@ char	*ft_strjoin(const char *s1, const char *s2)
 		newstr[j++] = s2[i++];
 	newstr[j] = 0;
 	return (newstr);
-}
-void	*ft_memcpy(void *dst, const void *src, size_t n)
-{
-	size_t		index;
-	char		*de;
-	char		*sr;
-
-	index = 0;
-	de = (char *)dst;
-	sr = (char *)src;
-	if (dst == NULL && src == NULL)
-		return (NULL);
-	while (index < n)
-	{
-		de[index] = sr[index];
-		index++;
-	}
-	return (dst);
 }
