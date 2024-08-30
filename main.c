ada0f18e3c53bf16c1c3b3bf5277dedf46dd4f29
Goullios <ngoulios@outlook.com>
2024-08-30 17:59:14 +0300

    clean

--- main.c
+++ main.c
@@ -5,39 +5,6 @@
 #include <limits.h>
 #include "get_next_line.h" 
 
-/*int main(int argc, char **argv)
-{
-    int     fd;
-    char    *line;
-
-    if (argc != 2)
-    {
-        printf("Usage: %s <file>\n", argv[0]);
-        return (1);
-    }
-
-    fd = open(argv[1], O_RDONLY);
-    if (fd < 0)
-    {
-        perror("Error opening file");
-        return (1);
-    }
-
-    while ((line = get_next_line(fd)) != NULL)
-    {
-        printf("%s", line);
-        free(line);
-    }
-
-    if (close(fd) < 0)
-    {
-        perror("Error closing file");
-        return (1);
-    }
-
-    return (0);
-}*/
-
 int main(int argc, char **argv)
 {
     int fd, ret, line_count;
